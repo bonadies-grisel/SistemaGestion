654942e5c0ba014ab4866a42b7119356280db583
Grisel <bonadies.grisel@gmail.com>
2022-09-07 12:49:50 -0300

    Commit

---
+++ Controllers/NombreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    [ApiController]
+    [Route("/api/[Controller]")]   
+    public class NombreController : ControllerBase
+    {
+        /
+        [HttpGet]
+        public string GetNombreLocal()
+        {
+            return "El nombre que quieras";
+        }        
+    }
+}
